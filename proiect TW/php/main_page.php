99a0d7d628e1eecdde6d15bf2d5afb5424e085dc
Roxana Portase <64905280+Roxxz@users.noreply.github.com>
2021-06-07 16:06:51 +0300

    refactor donate

--- proiect TW/php/main_page.php
+++ proiect TW/php/main_page.php
@@ -4,6 +4,9 @@
 <head>
     <title>ConGratZ</title>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/page_styles.css">
     <link rel="stylesheet" href="../css/scroll.css">
     <link rel="stylesheet" href="../menu/menu_bar.css">
@@ -12,6 +15,7 @@
 </head>
 
 <body>
+
 <?php include("../menu/menu_bar.html"); ?>
 
     <img class="welcome" src="../assets/welcome.png" alt="welcome">
