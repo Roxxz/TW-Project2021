583718e68cc66852c9c3b4cde45eb9f261dae861
AnaMargaretaJuncu(E3) <79104832+AnaMargaretaJuncu@users.noreply.github.com>
2021-06-08 15:39:22 +0300

    accounts css modified

--- proiect TW/php/accounts.php
+++ proiect TW/php/accounts.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="../css/page_styles.css">
     <link rel="stylesheet" href="../css/scroll.css">
     <link rel="stylesheet" href="../menu/menu_bar.css">
-    <link rel="stylesheet" href="../css/buttons.css">
     <link rel="stylesheet" href="../css/account.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 </head>
@@ -16,37 +15,43 @@
 <?php include("../menu/menu_bar.html"); ?>
 
     <h1>Our team</h1>
-
-    <p>
-        Hello there! In this section you will find some links in case you want to contact directly one of our founding members.
-    </p>
-    <p>
-        You can also fill in the form you'll find
-        <form style="display: inline" action="../contact/form.php" method="get">
-            <button class="dropbtn">here</button> to let us know what you think about us
-        </form>
-        <br>
-        <div>
-
-            <p>Founding members:</p>
-
-            <p></p>
-            <form style="display: inline" action="https://www.facebook.com/roxana.nicoleta.712" method="get">
+<p>
+    Hello there! In this section you will find some links in case you want to contact directly one of our founding members.
+</p>
+<p>
+    You can also fill in the form you'll find
+<form style="display: inline" action="contact_form.html" method="get">
+    <button class="dropbtn">here</button> to let us know what you think about us!
+</form>
+</p>
+<br>
+<div>
+    <hr>
+    <h2>Founding members:</h2>
+    <div class="grid-container">
+        <form style="display: inline" action="https://www.facebook.com/roxana.nicoleta.712" method="get">
+            <div class="picItem">
+                <img src="../assets/1.jpg">
                 <button class="dropbtn">Roxana Portase</button>
-            </form>
+            </div>
+        </form>
 
-            <p></p>
-
-            <form style="display: inline" action="https://www.facebook.com/andreealuca25" method="get">
+        <form style="display: inline" action="https://www.facebook.com/andreealuca25" method="get">
+            <div class="picItem">
+                <img src="../assets/2.jpg">
                 <button class="dropbtn">Andreea Luca</button>
-            </form>
+            </div>
+        </form>
 
-            <p></p>
 
-            <form style="display: inline" action="https://www.facebook.com/anamargareta.juncu" method="get">
+        <form style="display: inline" action="https://www.facebook.com/anamargareta.juncu" method="get">
+            <div class="picItem">
+                <img src="../assets/3.jpg">
                 <button class="dropbtn">Ana Juncu</button>
-            </form>
-        </div>
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 
 </html>
